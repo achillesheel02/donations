7cbc853470c6a8f85c2554e9153396bfd4d1de9b
Barak <bachillah@gmail.com>
2020-03-30 00:02:04 +0300

    adding login and signup

--- backend/models/blog.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-
-}
-);
-
-module.exports = mongoose.model('Blog',blogSchema);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import {Menu20Module} from '@carbon/icons-angular/lib/menu/20';
 import {Close20Module} from '@carbon/icons-angular/lib/close/20';
 import {HeaderModule} from 'carbon-components-angular';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
